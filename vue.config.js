3667f36a959281bd8c26dd710912896a85204c1a
BotirRuziyev <botirr735@gmain.com>
2024-07-21 10:05:12 +0500

    fejbfwkje

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  outputDir: "dist",
+  assetsDir: "static",
+  devServer: {
+    proxy: "http://localhost:8080",
+  },
+};
